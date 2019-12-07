dcb29bcf18760670b5f76ddd4cb46e4baa1332ef
Piotr Szyma <thompson2908@gmail.com>
2019-12-08 00:52:52 +0100

    day4 task1

---
+++ day4/task1.py
@@ -0,0 +1,15 @@
+pswd_min, pswd_max = 264793, 803935
+
+
+def meets_criteria(num):
+    num = str(num)
+    adjacent = False
+    for l, r in zip(num[:-1], num[1:]):
+        if int(l) > int(r):
+            return False
+        if l == r and not adjacent:
+            adjacent = True
+    return adjacent
+
+
+print(sum(1 for num in range(pswd_min, pswd_max + 1) if meets_criteria(num)))
